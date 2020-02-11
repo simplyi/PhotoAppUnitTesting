cd1557ed2b67fc36ade48e079cbdc39b4a0f44d9
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-02-11 10:35:55 -0500

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Sergey Kargopolov on 2020-02-11.
+//  Copyright © 2020 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
