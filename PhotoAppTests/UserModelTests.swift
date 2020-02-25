c6043df1515d9bbb58cb60deff60267b2a383cac
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-02-25 10:48:48 -0500

    Created a new project

---
+++ PhotoAppTests/UserModelTests.swift
@@ -0,0 +1,33 @@
+//
+//  UserModelTests.swift
+//  PhotoAppTests
+//
+//  Created by Sergey Kargopolov on 2020-02-11.
+//  Copyright © 2020 Sergey Kargopolov. All rights reserved.
+//
+
+import XCTest
+
+class UserModelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
