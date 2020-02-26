fa88f15f753e6ac7ae15504ff6b540bdc53eaa89
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-02-26 13:15:19 -0500

    Unit tests methods to check for Min and Max length of user First Name

--- PhotoApp/SignupFormModelValidator.swift
+++ PhotoApp/SignupFormModelValidator.swift
@@ -14,7 +14,7 @@ class SignupFormModelValidator {
     func isFirstNameValid(firstName: String) -> Bool {
         var returnValue = true
         
-        if firstName.isEmpty {
+        if firstName.count < SignupConstants.firstNameMinLength || firstName.count > SignupConstants.firstNameMaxLength {
             returnValue = false
         }
         
--- PhotoAppTests/SignupFormModelValidatorTests.swift
+++ PhotoAppTests/SignupFormModelValidatorTests.swift
@@ -10,25 +10,45 @@ import XCTest
 @testable import PhotoApp
 
 class SignupFormModelValidatorTests: XCTestCase {
+    
+    var sut: SignupFormModelValidator!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = SignupFormModelValidator()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
 
     func testSignFormModelValidator_WhenValidFirstNameProvided_ShouldReturnTrue() {
         
         // Arrange
-        let sut = SignupFormModelValidator()
-        
         // Act
         let isFirstNameValid = sut.isFirstNameValid(firstName: "Sergey")
         
         // Assert
         XCTAssertTrue(isFirstNameValid, "The isFirstNameValid() should have returned TRUE for a valid first name but returned FALSE")
+    }
+    
+    func testSignupFormModelValidator_WhenTooShortFirstNameProvided_ShouldReturnFalse() {
+        
+        // Arrange
+        // Act
+        let isFirstNameValid = sut.isFirstNameValid(firstName: "S")
+        
+        // Assert
+        XCTAssertFalse(isFirstNameValid, "The isFirstNameValid() should have returned FALSE for a first name that is shorter than 2 characters but it has returned TRUE")
+        
+    }
+    
+    func testSignupFormModelValidator_WhenTooLongFirstNameProvided_ShouldReturnFalse() {
+        
+        let isFirstNameValid = sut.isFirstNameValid(firstName: "SergeySergey")
+        
+        XCTAssertFalse(isFirstNameValid, "The isFirstNameValid() should have returned FALSE for a first name that is longer than 10 characters but it has returned TRUE")
     }
 }
