6b5715165063e8c045862106d6551efc1fb51314
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-03-19 10:37:36 -0400

    Make the SignupModelValidator and the Mock class conform to a Validator protocol

--- PhotoAppTests/Signup/MockSignupModelValidator.swift
+++ PhotoAppTests/Signup/MockSignupModelValidator.swift
@@ -7,8 +7,32 @@
 //
 
 import Foundation
+@testable import PhotoApp
 
 
-class MockSignupModelValidator {
+class MockSignupModelValidator: SignupModelValidatorProtocol {
+    
     var isFirstNameValidated: Bool = false
+    
+    func isFirstNameValid(firstName: String) -> Bool {
+        isFirstNameValidated = true
+        return isFirstNameValidated
+    }
+    
+    func isLastNameValid(lastName: String) -> Bool {
+        return true
+    }
+    
+    func isValidEmailFormat(email: String) -> Bool {
+        return true
+    }
+    
+    func isPasswordValid(password: String) -> Bool {
+        return true
+    }
+    
+    func doPasswordsMatch(password: String, repeatPassword: String) -> Bool {
+        return true
+    }
+
 }
